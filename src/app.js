fbd5ab322c3d890b9d7df47d5efb7184e02b5518
leocuba88 <leocuba88@hotmail.com>
2021-07-13 23:44:55 +0000

    doind

--- src/app.js
+++ src/app.js
@@ -7,5 +7,27 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let card = document.querySelector(".card");
+  let cardNumbers = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "King",
+    "Queen",
+    "Ace"
+  ];
+  let cardSuits = ["heart", "spades", "clubs", "diamonds"];
+  let randomCardNumber =
+    cardNumbers[Math.floor(Math.random() * cardNumbers.length)];
+  let randomCardSuits = cardSuits[Math.floor(Math.random() * cardSuits.length)];
+  let cardSuit = document.querySelector(".card h1");
+  cardSuit.innerHTML = randomCardNumber;
+  card.classList.add(randomCardSuits);
 };
